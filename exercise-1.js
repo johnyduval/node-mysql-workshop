7ce31318b56d11d31b8e78379b4f7cca6388a662
Johny Junior Duval <johny.duval@gmail.com>
2015-11-26 18:40:38 +0000

    Exercise #1: Getting started!

--- exercise-1.js
+++ exercise-1.js
@@ -1,24 +1,27 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host     : process.env.IP,
-  user     : process.env.C9_USER,
+  host     : '127.0.0.1',
+  user     : 'johnyduval',
   password : '',
-  database : 'addressbook'
+  database : ''
 });
 
-connection.query("SELECT * FROM Account", function(err, rows, fields) {
+connection.query("show databases;", function(err, rows, fields) {
   // In this callback, rows will be all the rows of the query, in a regular array of regular objects
   // fields is not used very often, but it will contain a listing of the columns with some metadata
-  
+  if (err){
+    console.log(err);
+  } else {
   // Here is an example usage:
   rows.forEach(function(row) {
-    console.log('#' + row.id + ': ' + row.email);
-  });
+    console.log(row);
+    });
+  }
   // This code will output lines like:
   // #1: john@smith.com
   // #2: abc@def.com
   // #5: xx@yy.com
-  
+  connection.end();
   // Note that IDs do not have to be contiguous. If we DELETE rows, there will be holes in the ID list. This is normal.
 });
